46715f3bf950117327e52ead8aa4e539a56aaa05
jhangmez <jhangomez25@gmail.com>
2023-10-25 01:31:09 -0500

    Se modifico colores de la pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,123 @@
+'use client'
+
 import Image from 'next/image'
+import React from 'react'
+import { Icon } from '@iconify/react'
+import Link from 'next/link'
 
 export default function Home() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className='bg-light-surface dark:bg-dark-surface w-screen h-screen'>
+      <nav className='bg-light-surface dark:bg-dark-surface relative flex flex-wrap items-center justify-between'>
+        <div className='container mx-auto flex flex-wrap items-center justify-between'>
+          <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
+            <Link
+              href='/'
+              className='w-[230px] h-14 justify-start items-center gap-[5px] inline-flex'
+            >
+              <Icon
+                icon='material-symbols:sentiment-excited-outline'
+                width='28'
+                height='28'
+                className='text-black dark:text-white '
+              />
+              <div>
+                <span className='text-black dark:text-white text-2xl font-bold leading-[44px]'>
+                  jhan
+                </span>
+                <span className='text-light-primary dark:text-dark-primary text-2xl font-bold leading-[44px]'>
+                  gmez
+                </span>
+              </div>
+            </Link>
+            <div className='lg:hidden flex flex-row items-center gap-2.5'>
+              <button
+                className='text-light-onSurface dark:text-dark-onSurface cursor-pointer text-xl leading-none py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
+                type='button'
+                onClick={() => setNavbarOpen(!navbarOpen)}
+              >
+                <Icon icon='material-symbols:menu' />
+              </button>
+            </div>
+          </div>
+          <div
+            className={
+              'lg:flex flex-grow items-center' +
+              (navbarOpen ? ' flex' : ' hidden')
+            }
+            id='navbar'
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <ul className='flex flex-col lg:flex-row list-none lg:ml-auto'>
+              <li className='nav-item'>
+                <div className='w-[fit] h-10 rounded-[100px] flex-col justify-center items-center gap-2 inline-flex'>
+                  <div className='self-stretch grow shrink basis-0 px-3 py-2.5 justify-center items-center gap-2 inline-flex'>
+                    <div className="text-center text-light-primary dark:text-dark-primary text-sm font-medium font-['Plus Jakarta Sans'] leading-tight tracking-tight">
+                      Skills
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className='nav-item'>
+                <div className='w-[fit] h-10 rounded-[100px] flex-col justify-center items-center gap-2 inline-flex'>
+                  <div className='self-stretch grow shrink basis-0 px-3 py-2.5 justify-center items-center gap-2 inline-flex'>
+                    <div className="text-center text-light-primary dark:text-dark-primary text-sm font-medium font-['Plus Jakarta Sans'] leading-tight tracking-tight">
+                      Skills
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className='nav-item'>
+                <div className='w-[fit] h-10 rounded-[100px] flex-col justify-center items-center gap-2 inline-flex'>
+                  <div className='self-stretch grow shrink basis-0 px-3 py-2.5 justify-center items-center gap-2 inline-flex'>
+                    <div className="text-center text-light-primary dark:text-dark-primary text-sm font-medium font-['Plus Jakarta Sans'] leading-tight tracking-tight">
+                      Skills
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className='nav-item'>
+                <div className='w-[fit] h-10 rounded-[100px] flex-col justify-center items-center gap-2 inline-flex'>
+                  <div className='self-stretch grow shrink basis-0 px-3 py-2.5 justify-center items-center gap-2 inline-flex'>
+                    <div className="text-center text-light-primary dark:text-dark-primary text-sm font-medium font-['Plus Jakarta Sans'] leading-tight tracking-tight">
+                      Skills
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className='nav-item'>
+                <div className='w-[fit] h-10 rounded-[100px] flex-col justify-center items-center gap-2 inline-flex'>
+                  <div className='self-stretch grow shrink basis-0 px-3 py-2.5 justify-center items-center gap-2 inline-flex'>
+                    <div className="text-center text-light-primary dark:text-dark-primary text-sm font-medium font-['Plus Jakarta Sans'] leading-tight tracking-tight">
+                      Skills
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Link
+        href='/'
+        className=' justify-start items-center gap-[5px] inline-flex'
+      >
+        <Icon
+          icon='material-symbols:sentiment-sad-outline'
+          width='28'
+          height='28'
+          className='text-black dark:text-white '
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div>
+          <span className='text-black dark:text-white text-2xl font-bold leading-[44px]'>
+            Página{' '}
+          </span>
+          <span className='text-light-primary dark:text-dark-primary text-2xl font-bold leading-[44px]'>
+            en construcción
+          </span>
+        </div>
+      </Link>
     </main>
   )
 }
