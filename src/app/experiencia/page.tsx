0ee8161bbb5ebdb0352417a9e28721423309acf3
jhangmez <jhangomez25@gmail.com>
2023-10-28 13:05:32 -0500

    se agrego header y footer en skills y experiencia

--- src/app/experiencia/page.tsx
+++ src/app/experiencia/page.tsx
@@ -1,3 +1,15 @@
-export default function Experiencias() {
-  return <div>HGola estas son mis experiencias :D</div>
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+export default function Experiencia() {
+  return (
+    <main className='w-screen h-screen'>
+      <div className='bg-light-surface dark:bg-dark-surface '>
+        <Header />
+      </div>
+      Hola estas son mis experiencias :D
+      <div className='bg-light-surface dark:bg-dark-surface'>
+        <Footer />
+      </div>
+    </main>
+  )
 }
