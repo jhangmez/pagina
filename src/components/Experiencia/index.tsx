3366bf719e3af2ce6fa84e18bc5ed2677321a87c
jhangmez <jhangomez25@gmail.com>
2023-10-28 12:34:11 -0500

    Se arreglo los tabs

--- src/components/Experiencia/index.tsx
+++ src/components/Experiencia/index.tsx
@@ -3,6 +3,9 @@
 import React, { useState } from 'react'
 import Tabs from '@components/Tabs'
 import TabContent from '@components/Tabs/TabContent'
+import Tab1 from './Tab1'
+import Tab2 from './Tab2'
+import Tab3 from './Tab3'
 
 export default function Experiencia() {
   const tabs = [
@@ -17,34 +20,13 @@ export default function Experiencia() {
         <h1 className='text-2xl font-bold '>Experiencia</h1>
         <Tabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab}>
           <TabContent key='1' id='1' activeTab={activeTab}>
-            <h2 className='text-lg font-bold '>Bartebi</h2>
-            <p className='text-sm font-medium '>Posición: React Developer</p>
-            <p className='text-sm font-medium '>Responsabilidades:</p>
-            <ul className='list-disc list-inside text-sm font-medium '>
-              <li>Desarrollo de aplicaciones web</li>
-              <li>Mantenimiento de la base de código</li>
-              <li>Colaboración en el diseño de la arquitectura del software</li>
-            </ul>
+            <Tab1 />
           </TabContent>
           <TabContent key='2' id='2' activeTab={activeTab}>
-            <h2 className='text-lg font-bold '>Limby</h2>
-            <p className='text-sm font-medium '>Posición: React Developer</p>
-            <p className='text-sm font-medium '>Responsabilidades:</p>
-            <ul className='list-disc list-inside text-sm font-medium '>
-              <li>Desarrollo de aplicaciones web</li>
-              <li>Mantenimiento de la base de código</li>
-              <li>Colaboración en el diseño de la arquitectura del software</li>
-            </ul>
+            <Tab2 />
           </TabContent>
           <TabContent key='3' id='3' activeTab={activeTab}>
-            <h2 className='text-lg font-bold '>HarkaySoft</h2>
-            <p className='text-sm font-medium '>Posición: React Developer</p>
-            <p className='text-sm font-medium '>Responsabilidades:</p>
-            <ul className='list-disc list-inside text-sm font-medium '>
-              <li>Desarrollo de aplicaciones web</li>
-              <li>Mantenimiento de la base de código</li>
-              <li>Colaboración en el diseño de la arquitectura del software</li>
-            </ul>
+            <Tab3 />
           </TabContent>
         </Tabs>
       </div>
