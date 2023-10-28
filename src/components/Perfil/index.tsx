79772dfd95493f734e31f7e52fffae3feb5e41b6
jhangmez <jhangomez25@gmail.com>
2023-10-28 17:36:37 -0500

    Se modifco los objetivos profesionales

--- src/components/Perfil/index.tsx
+++ src/components/Perfil/index.tsx
@@ -10,7 +10,7 @@ export default function CardWithActionButton() {
     <div className='container mx-auto lg:px-16'>
       <div className='flex h-full flex-col justify-center px-3 lg:px-0'>
         <div className='flex flex-col lg:flex-row justify-between items-center inline-flex py-10 gap-5'>
-          <div className='lg:w-4/12 w-11/12 order-2 lg:order-1 h-auto py-3 bg-light-surfaceContainer dark:bg-dark-surfaceContainer rounded-[20px] shadow flex-col justify-start items-center gap-2 inline-flex'>
+          <div className='lg:w-4/12 w-11/12 h-auto py-3 bg-light-surfaceContainer dark:bg-dark-surfaceContainer rounded-[20px] shadow flex-col justify-start items-center gap-2 inline-flex'>
             <div className='px-2 h-auto text-2xl text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant font-bold'>
               Jhan Gómez P.
             </div>
@@ -35,8 +35,75 @@ export default function CardWithActionButton() {
               <Navitem label='Github' icon='mdi:github' href={GithubJhan} />
             </div>
           </div>
-          <div className='text-4xl lg:text-5xl font-bold order-1 lg:order-2 text-light-onSurface dark:text-dark-onSurface'>
-            React Junior Frontend Developer
+          <div className='lg:w-7/12 grid flex-col text-light-onSurface dark:text-dark-onSurface gap-5'>
+            <div className='text-4xl lg:text-5xl font-bold '>
+              React Junior Frontend Developer
+            </div>
+            <div className='text-2xl lg:text-3x1 font-semibold'>
+              Objetivos profesionales
+            </div>
+            <div className='text-1xl lg:text-3x1 font-normal'>
+              Desarrollar mis habilidades y conocimientos como desarrollador{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                front-end
+              </span>{' '}
+              en{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                React
+              </span>
+              ,{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                Vue
+              </span>{' '}
+              y{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                Angular
+              </span>{' '}
+              . En el corto plazo, me centraré en desarrollar una aplicación web
+              escalable y de alto rendimiento utilizando{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                React
+              </span>{' '}
+              con{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                Next.js
+              </span>
+              . En el mediano plazo, espero convertirme en un desarrollador{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                Front-end Senior
+              </span>{' '}
+              y apoyar a nuevos programadores en tecnologías{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                nuevas
+              </span>{' '}
+              y{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                escalables
+              </span>{' '}
+              , para contribuir al desarrollo profesional de la comunidad. Creo
+              que mis habilidades y experiencia me hacen un candidato ideal para
+              apoyar como{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                Junior Developer
+              </span>{' '}
+              en empresas que requieran, ya que estoy familiarizado con las{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                mejores prácticas
+              </span>{' '}
+              de desarrollo{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                front-end
+              </span>{' '}
+              , tengo experiencia trabajando en equipos de{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                desarrollo ágiles
+              </span>{' '}
+              y estoy comprometido con el{' '}
+              <span className='text-light-primary dark:text-dark-primary'>
+                desarrollo profesional continuo
+              </span>
+              .
+            </div>
           </div>
         </div>
       </div>
