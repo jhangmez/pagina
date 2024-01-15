eb63841bd38f569f949d30f9ea3cd59fdedb9cf1
jhangmez <jhangomez25@gmail.com>
2024-01-14 20:15:41 -0500

    Se modifico antes decia graphql clown

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,4 +6,4 @@ export const LinkedinJhan = 'https://www.linkedin.com/in/jhangmez'
 export const VercelHarkaysoft = 'https://harkaysoft.vercel.app/'
 export const VercelHanpy = 'https://hanpy.vercel.app/'
 export const VercelJhan = 'https://jhangmez.vercel.app/'
-export const URLCV = process.env.NEXT_PUBLIC_GRAPHQL
+export const URLCV = process.env.NEXT_PUBLIC_URLCV
