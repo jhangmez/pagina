adeb04e873c6bff6c2cfb2a18dc0ce0af48788d6
jhangmez <jhangomez25@gmail.com>
2024-04-30 18:08:44 -0500

    Se envio el svg

--- src/components/email/CV/index.tsx
+++ src/components/email/CV/index.tsx
@@ -1,11 +1,48 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text
+} from '@react-email/components'
 interface EmailTemplateProps {
   firstName: string
 }
 
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : ''
+
 export const EmailCV: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName
 }) => (
-  <div>
+  <Html>
+    <Head />
+    <Preview>😄 jhan gmez</Preview>
+    <Img
+      src={`${baseUrl}/icons/jhangmez.svg`}
+      width='28'
+      height='28'
+      alt='jhangmez_ico'
+    />
+    <div>
+      <span className='text-light-onSurface dark:text-dark-onSurface text-2xl font-bold leading-[44px]'>
+        jhan
+      </span>
+      <span className='text-light-primary dark:text-dark-primary text-2xl font-bold leading-[44px]'>
+        gmez
+      </span>
+    </div>
     <h1>Hola {firstName}, este es mi CV.</h1>
-  </div>
+  </Html>
 )
+
+const main = {
+  backgroundColor: '#F9FAF4',
+  fontFamily: 'Plus Jakarta Sans'
+}
