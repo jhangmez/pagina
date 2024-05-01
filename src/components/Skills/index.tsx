2276c085de255a7122ed524cebd52c49c7a39758
jhangmez <jhangomez25@gmail.com>
2024-05-01 18:51:12 -0500

    Se cambio skills list

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -6,30 +6,30 @@ export default function Skills() {
   return (
     <>
       <div className='bg-light-surfaceContainer dark:bg-light-secondaryContainer border rounded-xl shadow-md p-4 w-full'>
-        <h1 className='font-bold text-2xl'>Skills</h1>
-        <h2 className='font-bold text-1xl mb-2'>Tech Stack</h2>
-        <ul className='space-x-2'>
+        <h1 className='font-bold text-2xl pb-2'>Skills</h1>
+        <h2 className='px-1 font-bold text-1xl mb-2'>Tech Stack</h2>
+        <ul className='px-2 flex flex-wrap gap-1'>
           {skills.stack.map((num) => {
             const Component = skillsMap[num]
             return <Component key={num} />
           })}
         </ul>
-        <h2 className='font-bold text-1xl mb-2'>Developer tools</h2>
-        <ul className='space-x-2'>
+        <h2 className='px-1 font-bold text-1xl mb-2'>Developer tools</h2>
+        <ul className='px-2 flex flex-wrap gap-1'>
           {skills.tools.map((num) => {
             const Component = skillsMap[num]
             return <Component key={num} />
           })}
         </ul>
-        <h2 className='font-bold text-1xl mb-2'>APIs integrations</h2>
-        <ul className='space-x-2'>
+        <h2 className='px-1 font-bold text-1xl mb-2'>APIs integrations</h2>
+        <ul className='px-2 flex flex-wrap gap-1'>
           {skills.api.map((num) => {
             const Component = skillsMap[num]
             return <Component key={num} />
           })}
         </ul>
-        <h2 className='font-bold text-1xl mb-2'>Desarrollo</h2>
-        <ul className='space-x-2'>
+        <h2 className='px-1 font-bold text-1xl mb-2'>Desarrollo</h2>
+        <ul className='px-2 flex flex-wrap gap-1'>
           {skills.others.map((num) => {
             const Component = skillsMap[num]
             return <Component key={num} />
