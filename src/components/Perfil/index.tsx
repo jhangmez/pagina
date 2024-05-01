6d81ec1d6852e2b2142cf9fa511a7b299d1713d9
jhangmez <jhangomez25@gmail.com>
2024-05-01 18:00:26 -0500

    Se cambio el titulo de software a frontend

--- src/components/Perfil/index.tsx
+++ src/components/Perfil/index.tsx
@@ -266,7 +266,7 @@ export default function PerfilPrueba() {
             </span>
           </h1>
           <h2 className='text-2xl md:text-3x1 lg:text-4x1 font-semibold text-light-onSurface dark:text-dark-onSurface'>
-            Estudiante de Ingeniería de Sistemas y Desarrollador software.
+            Estudiante de Ingeniería de Sistemas y Desarrollador Front-end.
           </h2>
 
           <div className='flex flex-row gap-5'>
