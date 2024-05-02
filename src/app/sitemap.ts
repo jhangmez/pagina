8a662bc40c9d1230d6d37515da2370d2fd4a0015
jhangmez <jhangomez25@gmail.com>
2024-05-02 16:24:25 -0500

    sE Actuializo sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -43,6 +43,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.6
+    },
+    {
+      url: 'https://jhangmez.xyz/terms',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6
     }
   ]
 }
