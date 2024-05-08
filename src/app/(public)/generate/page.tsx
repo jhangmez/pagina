97781d4eff587ea98082c9196d2185e1cf14d397
jhangmez <jhangomez25@gmail.com>
2024-05-08 12:00:57 -0500

    Se modifico cv generate page

---
+++ src/app/(public)/generate/page.tsx
@@ -0,0 +1,7 @@
+export default function Generate() {
+  return (
+    <section className='min-h-screen bg-light-surface dark:bg-dark-surface'>
+      <div className='container mx-auto px-[20px] pt-10'>ESTO ES GENERATE</div>
+    </section>
+  )
+}
