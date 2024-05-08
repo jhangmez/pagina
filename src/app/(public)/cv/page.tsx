0d8e6845e10416081e04bb5bcf3ce9ce33f84f82
jhangmez <jhangomez25@gmail.com>
2024-05-08 11:58:46 -0500

    Se modifico cv page

---
+++ src/app/(public)/cv/page.tsx
@@ -0,0 +1,7 @@
+export default function Cv() {
+  return (
+    <section className='min-h-screen bg-light-surface dark:bg-dark-surface'>
+      <div className='container mx-auto px-[20px] pt-10'>HOLA ESTO ES CV</div>
+    </section>
+  )
+}
